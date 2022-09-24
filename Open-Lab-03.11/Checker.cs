a5a365760c09ea24efd3e1da30426d9cefd9c24f
Marko <jantulamarko@outlook.com>
2022-09-24 14:52:58 +0200

    Update Checker.cs

--- Open-Lab-03.11/Checker.cs
+++ Open-Lab-03.11/Checker.cs
@@ -1,12 +1,22 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_03._11
 {
     public class Checker
     {
         public bool IsPalindrome(string str)
+
         {
-            throw new NotImplementedException();
-        } 
+            char[]chr  = str.ToCharArray();
+            Array.Reverse(chr);
+
+            rev = new string(chr);
+
+            if (str == rev)
+                return true;
+            else
+                return false;
+        }
+
     }
 }
